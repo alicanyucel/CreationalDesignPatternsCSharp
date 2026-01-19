e4463328deac8a1be166a8647e6a49ce2c6a550d
Ali Can Yücel <yucelalicandan@hotmail.com>
2026-01-19 20:55:55 +0300

    factory

---
+++ FactoryDesign/FactoryMethod.cs
@@ -0,0 +1,38 @@
+﻿namespace FactoryDesign;
+
+public abstract class Website
+{
+    public abstract void CreatePages();
+}
+public class WebSiteEcommerce :Website
+{
+    public override void CreatePages()
+    {
+        // E-ticaret sitesi sayfalarını oluşturma işlemleri
+    }
+}
+public class WebSiteBlog : Website
+{
+    public override void CreatePages()
+    {
+        // blog sayfası 
+    }
+}
+public abstract class WebSiteFactory
+{
+    public abstract Website CreateWebSite();
+}
+public class WebSiteFactoryEcommerce : WebSiteFactory
+{
+    public override Website CreateWebSite()
+    {
+        return new WebSiteEcommerce();
+    }
+}
+public class WebSiteFactoryBlog : WebSiteFactory
+{
+    public override Website CreateWebSite()
+    {
+        return new WebSiteBlog();
+    }
+}
---
+++ FactoryDesign/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using FactoryDesign;
+
+Console.WriteLine("Hello, World!");
+WebSiteFactory factory=new WebSiteFactoryEcommerce();
+factory.CreateWebSite();
+//WebSiteFactory factoryBlog=new WebSiteFactoryBlog();
+//factoryBlog.CreateWebSite();
